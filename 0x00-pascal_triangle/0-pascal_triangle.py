456d0eb83f6dfa6c5e50374ac2a6eebdc9eb5aef
NORTH89 <nordintata0@gmail.com>
2024-06-27 13:35:51 +0100

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,21 +4,17 @@ Pascal triangle
 """
 
 def pascal_triangle(n):
-    """returns the pascal triangle"""
-    base = [[1], [1, 1]]
-
+    """Pascal triangle"""
     if n <= 0:
         return []
-    elif n == 1:
-        return [[1]]
-    elif n == 2:
-        return base
-
-    start = 2
-    while start < n:
-        array_to_push = [1]
-        for i in range(1, start):
-            array_to_push.append(base[start - 1][i - 1] + base[start - 1][i])
-        base.append(array_to_push + [1])
-        start += 1
-    return base
+    
+    triangle = [[1]]
+    
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    
+    return triangle
